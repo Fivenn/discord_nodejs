575dc0fc01ee58be335d4e2c59563796618e400b
Loken3 <charles.picard3@gmail.com>
2019-06-10 12:32:41 +0200

    Update question14.sql

--- bdd/question14.sql
+++ bdd/question14.sql
@@ -1,1 +1,1 @@
-select pseudo from Utilisateur natural join EstMembre as A1, EstMembre as A2 where A1.sanction = A2.sanction and A1.id_serveur != A2.id_serveur and A1.id_utilisateur = A2.id_utilisateur;
+select pseudo from Utilisateur natural join EstMembre as A1, EstMembre as A2 where A1.sanction_atom = A2.sanction_raison and A1.sanction_raison = A2.sanction_atom and A1.id_serveur != A2.id_serveur and A1.id_utilisateur = A2.id_utilisateur;
