11598a7fd1a256ae0dde46c6d3c7176ee174bab8
Loken3 <charles.picard3@gmail.com>
2019-06-10 11:21:26 +0200

    Add sanction

--- bdd/BDD.sql
+++ bdd/BDD.sql
@@ -22,15 +22,24 @@ create table Role (
     primary key(position)
 );
 
+create table Sanction (
+    duree boolean,
+    raison varchar(100),
+    atom varchar(10),
+    primary key(raison,atom)
+);
+
 create table EstMembre (
     id_utilisateur integer,
     id_serveur integer,
-    sanction boolean,
     role integer,
+	sanction_raison varchar(100),
+    sanction_atom varchar(10),
     primary key(id_utilisateur,id_serveur,role),
     constraint utilisateur_membre foreign key (id_utilisateur) references Utilisateur(id),
     constraint serveur_membre foreign key (id_serveur) references Serveur(id),
-    constraint role_membre foreign key (role) references Role(position)
+    constraint role_membre foreign key (role) references Role(position),
+	constraint sanction_membre foreign key (sanction_raison,sanction_atom) references Sanction(raison,atom)
 );
 
 create table Salon (
@@ -40,6 +49,16 @@ create table Salon (
     id_serveur integer,
     primary key(nom,type),
     constraint serveur_salon foreign key (id_serveur) references Serveur(id)
+);
+
+create table SanctionSalon (
+    sanction_raison varchar(100),
+    sanction_atom varchar(10),
+    nom_salon varchar(30),
+    type_salon varchar(30),
+    primary key(sanction_raison,sanction_atom,nom_salon,type_salon),
+    constraint sanctionSalon_sanction foreign key (sanction_raison,sanction_atom) references Sanction(raison,atom),
+    constraint sanctionSalon_salon foreign key (nom_salon,type_salon) references Salon(nom,type)
 );
 
 create table Commande (
@@ -60,7 +79,6 @@ create table AttributionRole(
     constraint attr_commande_nom foreign key (nom_commande,atom_commande) references Commande(nom,atom)
 );
 
-
 insert into Utilisateur values(1,'bob','456',false,'0000000000');
 insert into Utilisateur values(2,'harry','677',false,'0000000000');
 insert into Utilisateur values(3,'Fiven','555',true,'0000000000');
@@ -70,18 +88,23 @@ insert into Utilisateur values(5,'Birlak','888',false,'0000000000');
 insert into Serveur values(1,'IMR',30,4);
 insert into Serveur values(2,'Return null',10,3);
 
+insert into Salon values('general','textuel','general',1);
+insert into Salon values('general','vocal','general',1);
+insert into Salon values('nsfw','vocal','adulte',2);
+
 insert into role values('delegue','bleu',2);
 insert into role values('sbire','rouge',3);
 insert into role values('modo','vert',1);
 
-insert into EstMembre values(1,2,null,3);
-insert into EstMembre values(3,2,null,1);
-insert into EstMembre values(4,1,null,2);
-insert into EstMembre values(4,2,null,3);
+insert into Sanction values(true,'relou','KICK');
 
-insert into Salon values('general','textuel','general',1);
-insert into Salon values('general','vocal','general',1);
-insert into Salon values('nsfw','vocal','adulte',2);
+insert into SanctionSalon values('relou','KICK','nsfw','vocal');
+
+insert into EstMembre values(1,2,3,'relou','KICK');
+insert into EstMembre values(3,2,1,null,null);
+insert into EstMembre values(4,1,2,null,null);
+insert into EstMembre values(4,2,3,null,null);
+
 
 insert into Commande values('kikoo','KICK','general','textuel');
 insert into Commande values('yo','KICK','general','vocal');
