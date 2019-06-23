3fc1e7752deae9a5cf1ba91ffeb0fdabf11440f4
Nicolas SUEUR <nicolas1.sueur@orange.com>
2019-06-23 18:42:59 +0200

    add some informations about how to process command

--- bot_discord.js
+++ bot_discord.js
@@ -23,12 +23,21 @@ botDiscord.on('ready', () => {
     console.log(`Logged in as ${botDiscord.user.tag}!`);
 });
 
-botDiscord.on('guildMemberAdd', member => {
-});
+// D[particule](user, reason)
+// D[particule](user, reason,channels)
+// T[particule](user, duration, reason)
+// T[particule](user, duration, reason,channels)
 
 botDiscord.on('message', msg => {
     if(msg.content === '-ban') {
-        const request = `select * from utilisateur, serveur, estmembre, role where pseudo = '${msg.member.user.username}' and id_utilisateur = utilisateur.id and serveur.nom = '${msg.guild.name}'`;
+        // Vérifier que la personne qui réalise la commande a assez de droits (position < 3)
+        // Vérifier que la personne qui subit la commande a le role pour (position > 5)
+        // Vérifier si la commande est définitive
+        // Si conditions vérifiées alors on update la BDD
+        // Prévenir dans l'auteur de la prise en compte de la commande
+        // Prévenir la personne sanctionnée de la prise en compte de la commande
+
+        // const request = `select * from utilisateur, serveur, estmembre, role where pseudo = '${msg.member.user.username}' and id_utilisateur = utilisateur.id and serveur.nom = '${msg.guild.name}'`;
         dbDiscord.query(request).then(res => {
             if(res.rows[0].role < 3) {
                 
