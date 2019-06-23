3c5ecfe5e47701763f6d40ef1c2a3704f36cac9b
Loken3 <charles.picard3@gmail.com>
2019-06-23 11:16:52 +0200

    Reccuperation des informations et envoi du message

--- index.js
+++ index.js
@@ -1,61 +1,88 @@
-var W3CWebSocket = require('websocket').w3cwebsocket;
-var https = require('https');
+//=============
+// Init discord
+//=============
+const Discord = require('discord.js');
+const botDiscord = new Discord.Client();
+const token = 'NTg4MzQzODQ2OTcxMTEzNDky.XQnw-g.5Fi-UQwvOhmIlrhwsd8P04oFdZ0';
+botDiscord.login(token);
 
-var token = "NTg4MzQzODQ2OTcxMTEzNDky.XQjF5A.WM3z7lrV6iacw7OWVUGyFb5-mZY";
-var ws = new W3CWebSocket("wss://gateway.discord.gg/?v=6&encoding=json");
-  
-ws.onopen = function() {
-    return console.log("OPEN!")
-}, ws.onerror = function(a) {
-    console.error(a), process.exit(1)
-}, ws.onclose = function(a) {
-    console.error(a), process.exit(1)
-}, ws.onmessage = function (a) {
-    var msg = a.data;
-    var data = JSON.parse(msg);
-    if (data.op == 10) {
-        console.log(data);
-        var payload = {
-             "token": token,
-             "properties": {
-                "$os": "browser",
-                "$browser": "chrome",
-                   "$device": "device"
-             },
-            "compress": false,
-            "large_threshold": 250,
-            "presence": {
-                  "status": "online",
-                  "since": 91879201,
-                  "afk": false
-             }
-         }
-         var bigPayload = {"op": 2, "d": payload};
-           ws.send(JSON.stringify(bigPayload));
-     } else if (data.op == 0 && data.t == "READY") {
-         console.log(data);
-     } else if(data.op == 0 && data.d.content == "-keanu"){
-        const content = JSON.stringify({
-            content: "You're breathtaking!"
-         });
+//==============
+// Init database
+//==============
+const { Client } = require('pg');
+const connectionString = 'postgresql://bot_discord:bot_discord@localhost:5555/bot_discord';
+const dbDiscord = new Client({
+    connectionString: connectionString,
+});
+dbDiscord.connect();
 
-         let req = https.request({
-            method: 'POST',
-            headers: {
-               "Content-Type": 'application/json',
-               "Authorization": 'Bot ' + token
-            },
-            hostname: "discordapp.com",
-            path: `/api/v6/channels/588438839018913798/messages`
-         }, (res) => {
-            console.log(res);
-         });
+//===============
+// Bot management
+//===============
+botDiscord.on('ready', () => {
+    console.log('Connection');
+});
 
-         req.on('error', (error) => {
-            console.log(error);
-         });
+botDiscord.on('guildCreate', () => {
+    var pseudo = null;
+    console.log('New guild joined');
+    var guild_list = [];
+    var compteur = 0;
+    botDiscord.guilds.forEach(element => {
+        guild_list.push(element).guild;
+        compteur++;
+    });
+    console.log((guild_list[compteur-1]).name);
+    const id_serveur = guild_list[compteur-1].id;
+    const nom_serveur = guild_list[compteur-1].name;
+    const capacite = 30;
+    const createur = guild_list[compteur-1].ownerID; 
 
-         req.write(content);
-         req.end();
+    botDiscord.users.forEach(element => {
+        if(element.id == createur) {
+            pseudo = element.username;
+        
+        }
+    });
+
+    console.log(pseudo);
+    
+    /*const request = 'insert into Serveur values('${msg.member.user.username}',null,IMR,30,4)';
+        dbDiscord.query(request).then(res => {
+            if(res.rows[0].role < 3) {
+                
+            }
+            console.log(res.rows);
+        }).catch(e => console.error(e.stack));*/
+    botDiscord.users.get(botDiscord.guilds.get(guild_list[compteur-1].id).ownerID).send("Votre token de connection :");
+});
+
+botDiscord.on('guildDelete', () => {
+    console.log('ByeBye');
+});
+
+
+botDiscord.on('guildMemberAdd', member => {
+});
+
+botDiscord.on('message', msg => {
+    if(msg.content === '-ban') {
+        const request = `select * from utilisateur, serveur, estmembre, role where pseudo = '${msg.member.user.username}' and id_utilisateur = utilisateur.id and serveur.nom = '${msg.guild.name}'`;
+        dbDiscord.query(request).then(res => {
+            if(res.rows[0].role < 3) {
+                
+            }
+            console.log(res.rows);
+        }).catch(e => console.error(e.stack));
     }
-}
+});
+
+
+
+// const text = 'SELECT * FROM estmembre'
+
+// dbDiscord.query(text)
+//   .then(res => {
+//     console.log(res.rows[0])
+//   })
+//   .catch(e => console.error(e.stack))
