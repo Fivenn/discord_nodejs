44c4f8560a7f71aa4a854b8014945fae28620b85
Nicolas SUEUR <nicolas1.sueur@orange.com>
2019-06-24 11:45:28 +0200

    add ban and kick command + request to check role

--- bdd/BDD.sql
+++ bdd/BDD.sql
@@ -120,3 +120,17 @@ insert into AttributionRole values(2,'wesh','MUTE');
 
  GRANT ALL PRIVILEGES ON TABLE attributionrole, Commande, estmembre, role, sanction, sanctionsalon, serveur, utilisateur TO bot_discord;
 
+==================================================================================
+-- Peuplement pour la commande BAN
+insert into Utilisateur values(1,'Fiven','5555',true,'0000000000');
+insert into Utilisateur values(2,'Birlak','6666',false,'0000000000');
+insert into Serveur values(1, 'NodeJS', 30, 1);
+insert into Salon values('general', 'textuel', 'general', 1);
+insert into role values('admin', 'bleu',1);
+insert into role values('sbire', 'rouge', 5);
+-- insert into Sanction values(false, null, 'relou', 'BAN');
+-- insert into SanctionSalon values('relou', 'KICK', 'nsfw', 'vocal');
+insert into EstMembre values(1,1,1,null,null);
+insert into EstMembre values(2,1,5,null,null);
+insert into Commande values('ban', 'BAN', 'general', 'textuel');
+insert into AttributionRole values(1, 'ban', 'BAN');
--- bot_discord.js
+++ bot_discord.js
@@ -1,14 +1,14 @@
-//=============
-// Init discord
-//=============
+//==============//
+// Init discord //
+//==============//
 const Discord = require('discord.js');
 const botDiscord = new Discord.Client();
 const token = 'NTg4MzQzODQ2OTcxMTEzNDky.XQnw-g.5Fi-UQwvOhmIlrhwsd8P04oFdZ0';
 botDiscord.login(token);
 
-//==============
-// Init database
-//==============
+//===============//
+// Init database //
+//===============//
 const { Client } = require('pg');
 const connectionString = 'postgresql://bot_discord:bot_discord@localhost:5555/bot_discord';
 const dbDiscord = new Client({
@@ -16,9 +16,9 @@ const dbDiscord = new Client({
 });
 dbDiscord.connect();
 
-//===============
-// Bot management
-//===============
+//================//
+// Bot management //
+//================//
 botDiscord.on('ready', () => {
     console.log(`Logged in as ${botDiscord.user.tag}!`);
 });
@@ -28,31 +28,55 @@ botDiscord.on('ready', () => {
 // T[particule](user, duration, reason)
 // T[particule](user, duration, reason,channels)
 
-botDiscord.on('message', msg => {
-    if(msg.content === '-ban') {
-        // Vérifier que la personne qui réalise la commande a assez de droits (position < 3)
-        // Vérifier que la personne qui subit la commande a le role pour (position > 5)
-        // Vérifier si la commande est définitive
-        // Si conditions vérifiées alors on update la BDD
-        // Prévenir dans l'auteur de la prise en compte de la commande
-        // Prévenir la personne sanctionnée de la prise en compte de la commande
-
-        // const request = `select * from utilisateur, serveur, estmembre, role where pseudo = '${msg.member.user.username}' and id_utilisateur = utilisateur.id and serveur.nom = '${msg.guild.name}'`;
-        dbDiscord.query(request).then(res => {
-            if(res.rows[0].role < 3) {
-                
+botDiscord.on('message', message => {
+    if(!message.guild) return;
+    
+    if(message.content.startsWith('!kick')) {
+        const user = message.mentions.users.first();
+        if(user) {
+            const member = message.guild.member(user);
+            if(member) {
+                member.kick('Optional reason that will display in the audit logs').then(() => {
+                    message.reply(`Successfully kicked ${user.tag}`);
+                }).catch(err => {
+                    message.reply('I was unable to kick the member');
+                    console.error(err);
+                });
+            } else {
+                message.reply('That user isn\'t in this guild');
             }
-            console.log(res.rows);
-        }).catch(e => console.error(e.stack));
+        } else {
+            message.reply('You didn\'t mention the user to kick');
+        }
+    }
+
+    if(message.content.startsWith('!ban')) {
+        const user = message.mentions.users.first();
+        if(user) {
+            const member = message.guild.member(user);
+            if(member) {
+                member.ban({
+                    reason: 'They were bad!',
+                }).then(() => {
+                    message.reply(`Successfully banned ${user.tag}`);
+                }).catch(err => {
+                    message.reply('I was unable to ban the member');
+                    console.error(err);
+                });
+            } else {
+                message.reply('That user isn\'t in this guild!');
+            }
+        } else {
+            message.reply('Uou didn\'t mention the user to ban!');
+        }
     }
 });
 
-
-
-// const text = 'SELECT * FROM estmembre'
-
-// dbDiscord.query(text)
-//   .then(res => {
-//     console.log(res.rows[0])
-//   })
-//   .catch(e => console.error(e.stack))
+const text = `SELECT * FROM estmembre join utilisateur on estmembre.id_utilisateur = utilisateur.id join serveur on estmembre.id_serveur = serveur.id where pseudo = 'Fiven' and serveur.nom = 'NodeJS'`;
+dbDiscord.query(text)
+  .then(res => {
+    if(res.rows[0].role < 3) {
+        console.log(res.rows);
+    }
+  })
+  .catch(e => console.error(e.stack))
