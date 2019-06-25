8bbd7569d102195d63d34ded0e7621934cc76be8
Loken3 <charles.picard3@gmail.com>
2019-06-25 09:55:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -51,6 +51,10 @@ botDiscord.on('guildCreate', () => {
     var test_user = null;
     var test_role = null;
     var test_channel = null;
+    var test_membre = null;
+    var role_membre_pos = 0;
+    var role_membre_nom = null;
+    var list_role_membre = null;
 
     dbDiscord.query(`Select id from serveur where id='${id_serveur}'`).then(res => {
         test_serveur = res.rowCount
@@ -65,6 +69,21 @@ botDiscord.on('guildCreate', () => {
             botDiscord.users.get(botDiscord.guilds.get(guild_list[compteur-1].id).ownerID).send("Votre token de connection : "+token);
         }
     }).catch(e => console.error(e.stack));
+
+    roles_list.forEach(element => {
+
+        dbDiscord.query(`Select nom,position from role where nom='${element.name}' and position='${element.position}'`).then(res => {
+            test_role = res.rowCount
+            if(test_role != 1) {
+                request = `insert into role values('${element.name}','${element.color}','${element.position}');`;
+                dbDiscord.query(request, (err, res) => {
+                    if (err) {
+                        console.log(err.stack)
+                    }
+                })
+            }   
+        }).catch(e => console.error(e.stack));
+    });
 
     membre_list.forEach(element => {
 
@@ -83,30 +102,36 @@ botDiscord.on('guildCreate', () => {
                     }
                 })
             }
-        }).catch(e => console.error(e.stack));
-    });
 
-    roles_list.forEach(element => {
-    
-        dbDiscord.query(`Select nom,position from role where nom='${element.name}' and position='${element.position}'`).then(res => {
-            test_role = res.rowCount
-            console.log(test_role);
-            if(test_role != 1) {
-                request = `insert into role values('${element.name}','${element.color}','${element.position}');`;
-                dbDiscord.query(request, (err, res) => {
+        }).catch(e => console.error(e.stack));
+
+        dbDiscord.query(`Select id_utilisateur from estMembre where id_utilisateur='${element.user.id}' and id_serveur= '${id_serveur}'`).then(res => {
+            test_membre = res.rowCount
+            if(test_membre != 1) {
+                list_role_membre = element.roles;
+            
+                list_role_membre.forEach(element => {
+                    if(element.position >= role_membre_pos) {
+                        role_membre_pos = element.position;
+                        role_membre_nom = element.name;
+                    }
+                })
+
+                dbDiscord.query(`insert into estMembre values('${element.user.id}','${id_serveur}','${role_membre_pos}','${role_membre_nom}',null,null);`, (err, res) => {
                     if (err) {
                         console.log(err.stack)
                     }
                 })
-            }   
-        }).catch(e => console.error(e.stack));
+                role_membre_pos = 0;
+                role_membre_nom = null;
+            }
+        })
     });
-    
+
     channel_list.forEach(element => {
 
         dbDiscord.query(`Select nom,type from salon where nom='${element.name}' and type='${element.type}'`).then(res => {
             test_channel = res.rowCount
-            console.log(test_channel);
             if(test_channel != 1) {
                 request = `insert into salon values('${element.name}','${element.type}','${element.category}','${id_serveur}');`;
                 dbDiscord.query(request, (err, res) => {
@@ -117,6 +142,8 @@ botDiscord.on('guildCreate', () => {
             }   
         }).catch(e => console.error(e.stack));
     });
+
+
 });
 
 botDiscord.on('guildDelete', () => {
