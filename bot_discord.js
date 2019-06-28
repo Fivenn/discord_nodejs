9cce7276c7fddb33da22a5992e00b5928f3436b9
Nicolas SUEUR <nicolas1.sueur@orange.com>
2019-06-28 10:05:45 +0200

    - implement discord anti-spam (version 0 honneur)

--- bot_discord.js
+++ bot_discord.js
@@ -8,6 +8,11 @@ const botDiscord = new Discord.Client();
 const token = 'NTg4MzQzODQ2OTcxMTEzNDky.XQnw-g.5Fi-UQwvOhmIlrhwsd8P04oFdZ0';
 botDiscord.login(token);
 
+//===================//
+// Discord anti-spam //
+//===================//
+const antispam = require('discord-anti-spam');
+
 //================//
 // Bot management //
 //================//
@@ -33,6 +38,8 @@ botDiscord.on('message', message => {
     if (message.content.startsWith('!deaf')) {
         botFunctions.deaf(message);
     }
+
+    botDiscord.emit('checkMessage', message);
 });
 
 botDiscord.on('guildCreate', () => {
@@ -41,4 +48,19 @@ botDiscord.on('guildCreate', () => {
 
 botDiscord.on('guildDelete', (guild) => {
     console.log('ByeBye');
+});
+
+botDiscord.on('ready', () => {
+    antispam(botDiscord, {
+        warnBuffer:3,
+        maxBuffer: 5,
+        interval: 2000,
+        warningMessage: "please stop spamming!",
+        banMessage: "CHEEEEEEH",
+        maxDuplicatesWarning: 7,
+        maxDuplicatesBan: 10,
+        deleteMessagesAfterBanForPastDays: 7,
+        exemptRoles: ["modo"],
+        exemptUsers: []
+    });
 });
